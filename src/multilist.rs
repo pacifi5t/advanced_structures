1e04a1faac12e06b1a7443217d268a369b9df891
Andrii Derhach <thequasar7@gmail.com>
2022-11-14 01:20:11 +0200

    Implement some basic methods as if for regular list, fix memory issues

--- src/multilist.rs
+++ src/multilist.rs
@@ -1,3 +1,4 @@
+use std::fmt::{Display, Formatter};
 use std::ptr::NonNull;
 
 struct Node<T> {
@@ -23,21 +24,86 @@ impl<T> Node<T> {
     }
 
     fn add_child(&mut self, elem: T) {
-        let node = Node::new(elem, self.level + 1);
-        let child: Option<NonNull<Node<T>>> = Some((&node).into());
+        let node = Box::new(Node::new(elem, self.level + 1));
+        let child: Option<NonNull<Node<T>>> = Some(Box::leak(node).into());
         self.child = child;
     }
 
     fn add_next(&mut self, elem: T) {
-        let node = Node::new(elem, self.level);
-        let next: Option<NonNull<Node<T>>> = Some((&node).into());
+        let node = Box::new(Node::new(elem, self.level));
+        let next: Option<NonNull<Node<T>>> = Some(Box::leak(node).into());
         self.next = next;
     }
 }
 
 impl<T> Multilist<T> {
-    fn new() -> Self {
+    pub fn new() -> Self {
         Multilist { head: None, max_level: 0, len: 0 }
+    }
+
+    pub fn push_front(&mut self, elem: T) {
+        let mut node = Box::new(Node::new(elem, 0));
+        node.next = self.head;
+
+        self.head = Some(Box::leak(node).into());
+        self.len += 1;
+    }
+
+    pub fn push_back(&mut self, elem: T) {
+        let node = Box::new(Node::new(elem, 0));
+        if self.len == 0 {
+            self.head = Some(Box::leak(node).into());
+        } else {
+            unsafe {
+                let mut iter = self.head.unwrap();
+                while !iter.as_ref().next.is_none() {
+                    iter = iter.as_ref().next.unwrap();
+                }
+                iter.as_mut().next = Some(Box::leak(node).into());
+            }
+        }
+        self.len += 1;
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.pop_front_node().map(|node| node.into_elem())
+    }
+
+    pub fn pop_back(&mut self) -> Option<T> {
+        self.pop_back_node().map(|node| node.into_elem())
+    }
+
+    fn pop_back_node(&mut self) -> Option<Box<Node<T>>> {
+        unsafe {
+            let mut ctr: isize = 0;
+            let mut iter = self.head;
+            while ctr < self.len as isize - 2 && !iter?.as_ref().next.is_none() {
+                iter = iter?.as_ref().next;
+                ctr += 1;
+            }
+
+            let prev = iter?.as_mut();
+            let tail = Box::from_raw(prev.next.unwrap_or(iter?).as_ptr());
+            prev.next = None;
+
+            self.len -= 1;
+
+            if self.len == 0 {
+                self.head = None;
+            }
+
+            Some(tail)
+        }
+    }
+
+    fn pop_front_node(&mut self) -> Option<Box<Node<T>>> {
+        self.head.map(|node| unsafe {
+            let node = Box::from_raw(node.as_ptr());
+            self.head = node.next;
+
+            self.len -= 1;
+            node
+        })
     }
 
     //TODO: Implement those:
@@ -49,5 +115,27 @@ impl<T> Multilist<T> {
     // fn clone
     // fn clear
     // fn display
+}
 
+impl<T> Display for Multilist<T> where T: Display {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[")?;
+        let mut iter = self.head;
+        while !iter.is_none() {
+            unsafe {
+                write!(f, "{}, ", iter.unwrap().as_ref().elem)?;
+                iter = iter.unwrap().as_ref().next;
+            }
+        }
+        // \x08 == \b (backspace), but the latter is unsupported
+        write!(f, "\x08\x08]")
+    }
+}
+
+impl<T> Drop for Multilist<T> {
+    fn drop(&mut self) {
+        while let Some(mut node) = self.pop_front_node() {
+            drop(node);
+        }
+    }
 }
