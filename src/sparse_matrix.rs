e24f44086c838431e306e8eae9c592e292f8a6f9
Andrii Derhach <thequasar7@gmail.com>
2022-12-19 16:59:17 +0200

    Implement sparsity calculation & axis iterator

--- src/sparse_matrix.rs
+++ src/sparse_matrix.rs
@@ -4,7 +4,7 @@ use std::ptr::NonNull;
 
 #[derive(Clone)]
 struct Node<T> {
-    value: Option<T>,
+    value: T,
     row: usize,
     col: usize,
     next_row: MaybeNone<Node<T>>,
@@ -14,7 +14,7 @@ struct Node<T> {
 impl<T> Node<T> {
     fn new(value: T, row: usize, col: usize) -> Self {
         Node {
-            value: Some(value),
+            value,
             row,
             col,
             next_col: None,
@@ -23,10 +23,13 @@ impl<T> Node<T> {
     }
 }
 
-impl<T> Default for Node<T> {
+impl<T> Default for Node<T>
+where
+    T: Default,
+{
     fn default() -> Self {
         Node {
-            value: None,
+            value: T::default(),
             row: usize::MAX,
             col: usize::MAX,
             next_row: None,
@@ -38,6 +41,53 @@ impl<T> Default for Node<T> {
 pub struct SparseMatrix<T: Default> {
     rows_vec: Vec<NonNull<Node<T>>>,
     cols_vec: Vec<NonNull<Node<T>>>,
+    size: usize,
+}
+
+pub struct AxisIter<T>
+where
+    T: Default + Clone,
+{
+    head: MaybeNone<Node<T>>,
+    axis: usize,
+    len: usize,
+    max_len: usize,
+}
+
+impl<T> Iterator for AxisIter<T>
+where
+    T: Default + Clone,
+{
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.len == 0 {
+            return None;
+        }
+
+        let elem = self.head.map(|node| unsafe {
+            let node = node.as_ref();
+            let (index, next) = match self.axis {
+                0 => (node.row, node.next_col),
+                1 => (node.col, node.next_row),
+                _ => panic!("axis can be only 0 or 1"),
+            };
+
+            if index == self.max_len - self.len {
+                self.head = next;
+                node.value.clone()
+            } else {
+                T::default()
+            }
+        });
+
+        self.len -= 1;
+        Some(elem.unwrap_or_default())
+    }
+
+    fn size_hint(&self) -> (usize, Option<usize>) {
+        (self.len, Some(self.len))
+    }
 }
 
 impl<T> SparseMatrix<T>
@@ -48,6 +98,7 @@ where
         Self {
             rows_vec: Self::create_empty_nodes_vec(rows),
             cols_vec: Self::create_empty_nodes_vec(cols),
+            size: 0,
         }
     }
 
@@ -62,10 +113,14 @@ where
         vec
     }
 
+    pub fn sparsity(&self) -> f64 {
+        let max_size = (self.rows_vec.len() * self.cols_vec.len()) as f64;
+        (max_size - self.size as f64) / max_size
+    }
+
     pub fn from_2d_vec(vec: Vec<Vec<T>>) -> Self {
         let cols = vec.iter().map(|v| v.len()).max_by(|x, y| x.cmp(y)).unwrap();
-
-        let matrix = Self::new(vec.len(), cols);
+        let mut matrix = Self::new(vec.len(), cols);
 
         for (i, v) in vec.iter().enumerate() {
             let mut prev = Some(matrix.rows_vec[i]);
@@ -76,6 +131,7 @@ where
 
                 unsafe { prev.unwrap().as_mut().next_row = current };
                 prev = current;
+                matrix.size += 1;
             }
         }
 
@@ -98,9 +154,27 @@ where
 
     pub fn get(&self, row: usize, col: usize) -> T {
         if let Some(node) = self.get_node(row, col) {
-            unsafe { node.as_ref().value.unwrap() }
+            unsafe { node.as_ref().value }
         } else {
             T::default()
+        }
+    }
+
+    pub fn row_iter(&self, row: usize) -> AxisIter<T> {
+        AxisIter {
+            head: unsafe { self.rows_vec[row].as_ref().next_row },
+            axis: 1,
+            len: self.cols_vec.len(),
+            max_len: self.cols_vec.len(),
+        }
+    }
+
+    pub fn col_iter(&self, col: usize) -> AxisIter<T> {
+        AxisIter {
+            head: unsafe { self.cols_vec[col].as_ref().next_col },
+            axis: 0,
+            len: self.rows_vec.len(),
+            max_len: self.rows_vec.len(),
         }
     }
 
@@ -139,13 +213,13 @@ where
         for i in 0..self.rows_vec.len() {
             for j in 0..self.cols_vec.len() {
                 if let Some(node) = self.get_node(i, j) {
-                    unsafe { write!(f, "{:?} ", node.as_ref().value.unwrap())? };
+                    unsafe { write!(f, "{:?} ", node.as_ref().value)? };
                 } else {
                     write!(f, "{:?} ", T::default())?;
                 }
             }
             writeln!(f)?;
         }
-        Ok(())
+        writeln!(f, "Sparsity: {:.2}", self.sparsity())
     }
 }
