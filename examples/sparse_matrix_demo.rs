26f645b210450e01cacb97624349f546229314e9
Andrii Derhach <thequasar7@gmail.com>
2022-12-20 19:27:29 +0200

    Finish demo for sparse matrix

--- examples/sparse_matrix_demo.rs
+++ examples/sparse_matrix_demo.rs
@@ -47,7 +47,11 @@ fn print_help() {
     println!("\t{:<42}Add to current matrix last [N] copy", "add [N]");
     println!("\t{:<42}Transpose current matrix", "tran");
     println!("\t{:<42}Create a copy of matrix", "clone");
-    // println!("\t{:<42}Create new matrix in interactive mode", "new");
+    println!(
+        "\t{:<42}Create new matrix with [rows]x[cols] shape or",
+        "new {[rows] [cols]}"
+    );
+    println!("\t{:<42}in interactive mode", "");
     println!("\t{:<42}Set a copy [N] as current matrix", "restore [N]");
     println!("\t{:<42}Exit the program", "exit");
 }
@@ -61,12 +65,13 @@ fn parse_args(buf: String, copies: &mut Vec<SparseMatrix<Item>>) -> Result<bool,
         "show" => show(copies, args)?,
         "mul" => mul(m, args)?,
         "add" => add(copies, args)?,
+        "set" => set(m, args)?,
         "tran" => tran(m),
         "clone" => {
             let copy = m.clone();
             copies.push(copy)
         }
-        // "new" => new(copies, args)?,
+        "new" => new(copies, args)?,
         "restore" => restore(copies, args)?,
         "exit" => return Ok(true),
         _ => return Err("unknown command or empty input".into()),
@@ -101,17 +106,73 @@ fn add(copies: &mut Vec<SparseMatrix<Item>>, args: Vec<&str>) -> Result<(), Box<
     Ok(())
 }
 
+fn set(m: &mut SparseMatrix<Item>, args: Vec<&str>) -> Result<(), Box<dyn Error>> {
+    check_args(4, args.len(), None)?;
+
+    let row = args[1].parse()?;
+    let col = args[2].parse()?;
+
+    if row >= m.rows() || col >= m.cols() {
+        return Err("incorrect parameter".into());
+    }
+
+    m.set(args[3].parse()?, row, col);
+    Ok(())
+}
+
 fn tran(m: &mut SparseMatrix<Item>) {
     *m = m.transposed();
     info(m);
 }
 
 fn new(copies: &mut Vec<SparseMatrix<Item>>, args: Vec<&str>) -> Result<(), Box<dyn Error>> {
-    // check_args(1, args.len(), None)?;
-    //
-    // copies.push(SparseMatrix::from_2d_vec());
-    // Ok(())
-    todo!()
+    match args.len() {
+        1 => {
+            println!("Type elements of the matrix row separated by ' '");
+            println!("Type <enter> to switch row");
+            println!("Type <enter> twice to finish");
+            copies.push(new_interactive()?);
+        }
+        3 => copies.push(SparseMatrix::new(args[1].parse()?, args[2].parse()?)),
+        _ => return Err("incorrect arguments count, check help".into()),
+    }
+
+    info(copies.last().unwrap());
+    Ok(())
+}
+
+fn new_interactive() -> Result<SparseMatrix<Item>, Box<dyn Error>> {
+    let mut buf = String::new();
+    let mut vec = Vec::new();
+    let mut parse_error = None;
+
+    loop {
+        stdin().read_line(&mut buf).unwrap();
+        buf = buf.trim().parse().unwrap();
+
+        if buf.is_empty() {
+            break;
+        }
+
+        let v: Vec<Item> = buf
+            .split(' ')
+            .map(|e| match e.parse() {
+                Ok(item) => item,
+                Err(err) => {
+                    parse_error = Some(err);
+                    Item::default()
+                }
+            })
+            .collect();
+        vec.push(v);
+        buf.clear();
+
+        if let Some(e) = parse_error {
+            return Err(e.into());
+        }
+    }
+
+    Ok(SparseMatrix::from_2d_vec(vec))
 }
 
 fn check_args(expected: usize, actual: usize, msg: Option<&str>) -> Result<(), &str> {
